25d48df25ac9ae51c49c465e8955756d18d15858
Trisha Reddy <105570654+trishavreddy@users.noreply.github.com>
2023-11-17 15:50:22 -0500

    Added ExecutorService to Main

--- Main.java
+++ Main.java
@@ -6,12 +6,35 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
-import java.time.Duration;
-import java.time.Instant;
 
 
 public class Main {
 
-   
+    public static int sum = 0;
+
+    public static void main(String[] args)
+    {
+        try{
+
+            ExecutorService executor = Executors.newFixedThreadPool(10);
+
+            List<Thread> threads = new ArrayList<>();
+            for(int i = 0; i < 100; i++)
+            {
+                threads.add(new Thread());
+            }
+            List<Future<Integer>> list = executor.invokeAll(threads);
+            for(Future<Integer> fut : list)
+            {
+                sum+=fut.get();
+            }
+            executor.shutdown();
+            
+            System.out.println("Sum: "+sum);
+
+        }catch(InterruptedException|ExecutionException ex){
+            System.out.println(ex.getMessage());
+        }
+    }
 
 }
