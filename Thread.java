201a4761140b812660a57b93953f7f71e0f59486
Trisha Reddy <105570654+trishavreddy@users.noreply.github.com>
2023-11-17 15:48:37 -0500

    Added Thread code

--- Thread.java
+++ Thread.java
@@ -1,5 +1,19 @@
-public class Thread
-{
+import java.util.concurrent.Callable;
 
-    
+public class Thread implements Callable<Integer>
+{
+    @Override
+    public Integer call() throws Exception {
+
+        
+        int count = 0;
+        for(int i = 0; i < 1000000; i++)
+        {
+            count++;
+        }
+        return count;
+        
+    }
+
+
 }
